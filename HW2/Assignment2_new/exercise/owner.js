fb998ff53e87632b83e4bbfab2a9b214e2fc32f6
106971001 <106971001@nccu.edu.tw>
2018-11-17 21:15:07 +0800

    get owner

--- HW2/Assignment2_new/exercise/owner.js
+++ HW2/Assignment2_new/exercise/owner.js
@@ -12,7 +12,7 @@ web3.eth.getAccounts().then(function (accounts) {
 
     // get contract owner
     // your code
-    bank.getOwner.call().send({
+    bank.getOwner.call({
         from: accounts[1]
     })
     .on('receipt', console.log)
